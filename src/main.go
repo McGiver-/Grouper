e32c4348573cea6ff9dde9037284e998ef663a8c
George_Kolampas <g.kolampas@gmail.com>
2016-08-19 23:09:14 -0400

    Connect to db and add user function

--- src/main.go
+++ src/main.go
@@ -3,8 +3,10 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"log"
+	"database/sql"
+	_ "github.com/lib/pq"
 	"net/http"
+	"log"
 )
 
 type Category struct {
@@ -12,13 +14,46 @@ type Category struct {
 	NbUsrs string `json:"nbusrs"`
 }
 
+type AddUserResponse struct{
+	Success bool `json:"success"`
+}
+
 func main() {
 
 	http.HandleFunc("/categories", George)
 	http.HandleFunc("/friends", FriendHandler)
-
+	http.HandleFunc("/addUser",addUser)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
+
+/* --------------------------------------- Handlers -------------------------------------------------- */
+func addUser(rw http.ResponseWriter, req *http.Request){
+	db, err := sql.Open("postgres", "postgresql://george@localhost:26257/accounts?sslmode=disable")
+	if err != nil {
+		log.Fatalf("error connection to the database: %s", err)
+	}
+
+	username := req.URL.Query().Get("username")
+	password := req.URL.Query().Get("password")
+
+	// Insert two rows into the "accounts" table.
+	if _, err := db.Exec(
+		"INSERT INTO accounts (username, password) VALUES ("+username+","+password+")"); err != nil {
+		log.Fatal(err)
+	}else{
+		fmt.Printf("Users %s added",username);
+		response := AddUserResponse{true}
+		if err := json.NewEncoder(rw).Encode(response); err != nil{
+			rw.WriteHeader(http.StatusConflict)
+			panic(err)
+		}else{
+			rw.WriteHeader(http.StatusOK)
+			rw.Header().Set("Content-Type","application/json; charset=UTF-8")
+			fmt.Println("Json success sent")
+		}
+	}
+}
+
 
 func FriendHandler(rw http.ResponseWriter, req *http.Request) {
 	fmt.Fprintf(rw, "Got friends")
